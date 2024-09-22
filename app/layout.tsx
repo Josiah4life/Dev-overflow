5bac7544b545ff15ab93c55e5cc52c215d97cb49
Josiah <93512794+Josiah4life@users.noreply.github.com>
2024-09-22 06:20:03 +0100

    002

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,33 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
-
+import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+
 import "./globals.css";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community-driven platform for asking and ansering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -16,13 +36,8 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is a piece of text</h1>
           {children}
         </body>
       </html>
