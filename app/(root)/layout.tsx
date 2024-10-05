4831e624e47731a387b90b05e75cf9728cc62b35
Josiah <93512794+Josiah4life@users.noreply.github.com>
2024-10-05 09:14:15 +0100

    003

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/shared/navbar/Navbar";
-import LeftSidebar from "@/components/shared/sidebar/LeftSidebar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
 import React from "react";
+import RightSidebar from "@/components/shared/RightSidebar";
 // import Page from "./ask-question/page";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -8,12 +9,12 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <>
       <main className="background-light850_dark100 relative">
         <Navbar />
-        <div className="mt-14 flex">
+        <div className="flex">
           <LeftSidebar />
           <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
             <div className="mx-auto w-full max-w-5xl">{children}</div>
           </section>
-          RightSidebar
+          <RightSidebar />
         </div>{" "}
         Toaster
       </main>
--- components/shared/sidebar/LeftSidebar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LeftSidebar = () => {
-  return <div className="text-blue-700">LeftSideBar</div>;
-};
-
-export default LeftSidebar;
