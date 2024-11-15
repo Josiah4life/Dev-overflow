3dea2e33a9324bb271a113584117d4da1e2b2978
Josiah <93512794+Josiah4life@users.noreply.github.com>
2024-11-15 01:51:53 +0100

    Eslint

--- types/index.d.ts
+++ types/index.d.ts
@@ -38,13 +38,13 @@ export interface Country {
 export interface ParamsProps {
   params: { id: string };
 }
-export interface SearchParamsProps {
-  searchParams: { [key: string]: string | undefined };
-}
-
 // export interface SearchParamsProps {
-//   searchParams: Record<string, string | undefined>;
+//   searchParams: { [key: string]: string | undefined };
 // }
+
+export interface SearchParamsProps {
+  searchParams: Record<string, string | undefined>;
+}
 
 export interface URLProps {
   params: { id: string };
